8d1d2b670566369f2921442ce29d26f743183d3c
X_AJ_X <88792057+XAJX179@users.noreply.github.com>
2024-11-07 11:34:43 +0530

    Style : Rubocop auto-correct

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,65 +1,60 @@
+# frozen_string_literal: true
+
+# Enumerable methods
 module Enumerable
   def my_each_with_index
-    i = 0;
-    while i < self.length
-      yield(self[i],i)
-      i+=1;
+    i = 0
+    while i < length
+      yield(self[i], i)
+      i += 1
     end
     self
   end
 
   def my_select
     new_arr = []
-    self.my_each do |elem|
-      if yield(elem)
-        new_arr << elem
-      end
+    my_each do |elem|
+      new_arr << elem if yield(elem)
     end
     new_arr
   end
 
-  def my_all?
-    new_arr = self.my_select do |elem|
-      yield(elem)
-    end
-    new_arr.length == self.length ? true : false
+  def my_all?(&)
+    new_arr = my_select(&)
+    new_arr.length == length
   end
 
-  def my_any?
-    new_arr = self.my_select do |elem|
-      yield(elem)
-    end
-    new_arr.length > 0 ? true : false
+  def my_any?(&)
+    new_arr = my_select(&)
+    new_arr.length.positive?
   end
 
-  def my_none?
-    new_arr = self.my_select do |elem|
-      yield(elem)
-    end
-    new_arr.length == 0 ? true : false
+  def my_none?(&)
+    new_arr = my_select(&)
+    new_arr.empty?
   end
 
-  def my_count
-    return self.length unless block_given?
+  def my_count(&)
+    return length unless block_given?
 
-    new_arr = self.my_select { |elem| yield(elem) }
+    new_arr = my_select(&)
     new_arr.length
   end
 
   def my_map
-    if block_given?
-      new_arr=[]
-      self.my_each do |elem|
-        new_arr << yield(elem)
-      end
-      new_arr
+    return unless block_given?
+
+    new_arr = []
+    my_each do |elem|
+      new_arr << yield(elem)
     end
+    new_arr
   end
 
   def my_inject(initial_value)
     accumulator = initial_value
-    self.my_each_with_index do |elem,index|
-      accumulator = yield(accumulator,elem)
+    my_each do |elem|
+      accumulator = yield(accumulator, elem)
     end
     accumulator
   end
@@ -71,10 +66,10 @@ end
 # to this method
 class Array
   def my_each
-    i = 0;
-    while i < self.length
+    i = 0
+    while i < length
       yield(self[i])
-      i+=1;
+      i += 1
     end
     self
   end
